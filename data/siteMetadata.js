79be158eaac6e9e017314bbbc4b4efe2326ca7c7
jeongeun-ezllabs <jeongeun@ezllabs.co.kr>
2025-09-09 12:22:40 +0900

    fix: siteMetadata 수정 및 프로젝트 이미지 추가

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '',
   language: 'ko-kr',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://jeongeun-dev.vercel.app',
+  siteRepo: 'https://github.com/JEONGEUN0204/jeongeun-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   email: 'wjddms9921@gmail.com',
